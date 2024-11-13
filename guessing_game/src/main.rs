551ee999037b539201f8bf0cefe4276dd6517bc7
Satoki Hata <satokihata@gmail.com>
2024-11-13 20:08:09 +0900

    add io

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    let mut guess: String = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("読み込みに失敗しました");
+    println!("You guessed: {}", guess);
 }
---
+++ guessing_game/target/debug/deps/guessing_game-5cae4e2047a713bf.d
@@ -0,0 +1,5 @@
+/Users/hatasatoki/Documents/rust-helloWorld/guessing_game/target/debug/deps/libguessing_game-5cae4e2047a713bf.rmeta: src/main.rs
+
+/Users/hatasatoki/Documents/rust-helloWorld/guessing_game/target/debug/deps/guessing_game-5cae4e2047a713bf.d: src/main.rs
+
+src/main.rs:
