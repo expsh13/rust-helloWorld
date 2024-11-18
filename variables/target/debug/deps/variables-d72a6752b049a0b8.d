048520c764edbe644f721c5541dc234f63f7baac
Satoki Hata <satokihata@gmail.com>
2024-11-18 12:57:25 +0900

    add new project

---
+++ variables/target/debug/deps/variables-d72a6752b049a0b8.d
@@ -0,0 +1,5 @@
+/Users/hatasatoki/Documents/rust-helloWorld/variables/target/debug/deps/libvariables-d72a6752b049a0b8.rmeta: src/main.rs
+
+/Users/hatasatoki/Documents/rust-helloWorld/variables/target/debug/deps/variables-d72a6752b049a0b8.d: src/main.rs
+
+src/main.rs:
