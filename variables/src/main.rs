999cf7538d8798f3efe569ec9999c22c7197a022
Satoki Hata <satokihata@gmail.com>
2024-11-21 12:39:36 +0900

    String型

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -7,5 +7,9 @@ fn main() {
     println!("The value of y is: {}", y);
 
     let r = "hello";
-    let s = String::from("hello");
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str()関数は、リテラルをStringに付け加える
+
+    println!("{}", s); // これは`hello, world!`と出力する
 }
