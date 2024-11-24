713e1e13b81a7efdb8445c961af14647f4139e61
Satoki Hata <satokihata@gmail.com>
2024-11-24 11:23:13 +0900

    所有権とスコープ

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,15 +1,7 @@
 fn main() {
-    let y = {
-        let x = 3;
-        x + 1
-    };
+    {
+        let s = String::from("hello"); // sはここから有効になる
 
-    println!("The value of y is: {}", y);
-
-    let r = "hello";
-    let mut s = String::from("hello");
-
-    s.push_str(", world!"); // push_str()関数は、リテラルをStringに付け加える
-
-    println!("{}", s); // これは`hello, world!`と出力する
+        // sで作業をする
+    }
 }
