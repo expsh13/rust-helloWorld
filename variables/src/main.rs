d2107725c638cdeec0cb9328470ad46cdb528069
Satoki Hata <satokihata@gmail.com>
2024-11-25 23:49:37 +0900

    スライス型

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,20 @@
-fn main() {
-    let reference_to_nothing = dangle();
+fn first_word(s: &str) -> &str {
+    let bytes: &[u8] = s.as_bytes(); //バイト配列に
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
 
-fn dangle() -> &String {
-    let s = String::from("hello");
+fn main() {
+    let s = String::from("hello world");
+    let my_string_literal = "hello world";
 
-    &s
+    let word = first_word(&s[..]);
+    let word = first_word(my_string_literal);
+    println!("the first word is: {}", word);
 }
