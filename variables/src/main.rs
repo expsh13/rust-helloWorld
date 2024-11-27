7496d8c96a381fb259cb42f43e66a2ef9e2e7a9b
Satoki Hata <satokihata@gmail.com>
2024-11-28 08:48:30 +0900

    match

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,36 @@
 #![allow(unused)]
 fn main() {
-    enum Option<T> {
-        Some(T),
-        None,
+    #[derive(Debug)] // すぐに州を点検できるように
+    enum UsState {
+        Alabama,
+        Alaska,
+        // ... などなど
     }
-    let some_number = Some(5);
-    let some_string = Some("a string");
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState),
+    }
 
-    let absent_number: Option<i32> = None;
+    // fn value_in_cents(coin: Coin) -> u32 {
+    //     match coin {
+    //         Coin::Penny => 1,
+    //         Coin::Nickel => 5,
+    //         Coin::Dime => 10,
+    //         Coin::Quarter => 25,
+    //     }
+    // }
+
+    fn value_in_cents(coin: Coin) -> u32 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("State quarter from {:?}!", state);
+                25
+            }
+        }
+    }
 }
