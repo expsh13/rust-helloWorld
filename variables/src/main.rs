bbb1d0c29ef031e682af31cff68089e49f826afe
Satoki Hata <satokihata@gmail.com>
2024-11-28 08:24:04 +0900

    Option

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,41 +1,11 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-    fn square(size: u32) -> Rectangle {
-        Rectangle {
-            width: size,
-            height: size,
-        }
-    }
-}
-
+#![allow(unused)]
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
-    };
+    enum Option<T> {
+        Some(T),
+        None,
+    }
+    let some_number = Some(5);
+    let some_string = Some("a string");
 
-    let square = Rectangle::square(3);
-
-    // rect1にrect2ははまり込む？
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    let absent_number: Option<i32> = None;
 }
