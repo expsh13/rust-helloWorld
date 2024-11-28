96c6fa3283eafe3cbe0c875e55212cfdb3628c52
Satoki Hata <satokihata@gmail.com>
2024-11-28 22:51:30 +0900

    match other

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -33,4 +33,22 @@ fn main() {
             }
         }
     }
+
+    let five = Some(5);
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+    let six = plus_one(five);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
 }
