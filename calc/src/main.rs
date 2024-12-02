b62727f472c4045065d3c64eed636f4d9afe16ec
Satoki Hata <satokihata@gmail.com>
2024-12-02 20:08:14 +0900

    最初の計算

--- calc/src/main.rs
+++ calc/src/main.rs
@@ -1,3 +1,25 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    for line in stdin().lines() {
+        // 行毎読み込み
+        let line = line.unwrap();
+        if line.is_empty() {
+            break;
+        }
+        // 空白で分割
+        let tokens: Vec<&str> = line.split(char::is_whitespace).collect();
+        let left: f64 = tokens[0].parse().unwrap();
+        let right: f64 = tokens[2].parse().unwrap();
+        let result = match tokens[1] {
+            "+" => left + right,
+            "-" => left - right,
+            "*" => left * right,
+            "/" => left / right,
+            _ => {
+                unreachable!()
+            }
+        };
+        println!(" -> {}", result);
+    }
 }
