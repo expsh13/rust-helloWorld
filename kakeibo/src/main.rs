28df1b8f45d5fe5f79d1cdae3637c482a9967096
Satoki Hata <satokihata@gmail.com>
2024-12-10 21:48:01 +0900

    サブコメントの作成

--- kakeibo/src/main.rs
+++ kakeibo/src/main.rs
@@ -1,11 +1,23 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[clap(version = "1.0")]
-struct Args {
-    arg1: String,
-    arg2: String,
+struct App {
+    #[clap(subcommand)]
+    command: Command,
 }
-
+#[derive(Subcommand)]
+enum Command {
+    /// 新しい口座を作る
+    New,
+    /// 口座に入金する
+    Deposit,
+    /// 口座から出金する
+    Withdraw,
+    /// CSVからインポートする
+    Import,
+    /// レポートを出力する
+    Report,
+}
 fn main() {
-    let _args = Args::parse();
+    let _args = App::parse();
 }
