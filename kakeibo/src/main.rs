88da2478f148f6083143b6460889e7734409580d
Satoki Hata <satokihata@gmail.com>
2024-12-11 23:13:57 +0900

    import

--- kakeibo/src/main.rs
+++ kakeibo/src/main.rs
@@ -1,6 +1,7 @@
 use chrono::NaiveDate;
 use clap::{Args, Parser, Subcommand};
-use csv::Writer;
+use csv::{Reader, Writer, WriterBuilder};
+use serde::{Deserialize, Serialize};
 use std::fs::OpenOptions;
 #[derive(Parser)]
 #[clap(version = "1.0")]
@@ -17,9 +18,9 @@ enum Command {
     /// 口座から出金する
     Withdraw(WithdrawArgs),
     /// CSVからインポートする
-    Import,
+    Import(ImportArgs),
     /// レポートを出力する
-    Report,
+    Report(ReportArgs),
 }
 #[derive(Args)]
 struct NewArgs {
@@ -83,14 +84,46 @@ impl WithdrawArgs {
         writer.flush().unwrap();
     }
 }
-
+#[derive(Args)]
+struct ImportArgs {
+    src_file_name: String,
+    dst_account_name: String,
+}
+impl ImportArgs {
+    fn run(&self) {
+        let open_option = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(format!("{}.csv", self.dst_account_name))
+            .unwrap();
+        let mut writer = WriterBuilder::new()
+            .has_headers(false)
+            .from_writer(open_option);
+        let mut reader = Reader::from_path(&self.src_file_name).unwrap();
+        for result in reader.deserialize() {
+            let record: Record = result.unwrap();
+            writer.serialize(record).unwrap();
+        }
+        writer.flush().unwrap();
+    }
+}
+#[derive(Serialize, Deserialize)]
+struct Record {
+    日付: NaiveDate,
+    用途: String,
+    金額: i32,
+}
+#[derive(Args)]
+struct ReportArgs {
+    files: Vec<String>,
+}
 fn main() {
     let args = App::parse();
     match args.command {
         Command::New(args) => args.run(),
         Command::Deposit(args) => args.run(),
         Command::Withdraw(args) => args.run(),
-        Command::Import => unimplemented!(),
-        Command::Report => unimplemented!(),
+        Command::Import(args) => args.run(),
+        Command::Report(args) => unimplemented!(),
     }
 }
