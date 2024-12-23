8c6075ab61c350e004bef93fe2c174da52e73d55
Satoki Hata <satokihata@gmail.com>
2024-12-23 23:21:14 +0900

    ここから

--- textbook/src/main.rs
+++ textbook/src/main.rs
@@ -1,3 +1,46 @@
+use std::iter::Iterator;
+
 fn main() {
-    println!("Hello, world!");
+    #[derive(Debug, Clone)]
+    enum List<T> {
+        Node { data: T, next: Box<List<T>> },
+        Nil,
+    }
+    impl<T> List<T> {
+        fn new() -> List<T> {
+            List::Nil
+        }
+
+        // リストを消費して、そのリストの先頭に引数dataを追加したリストを返す
+        fn cons(self, data: T) -> List<T> {
+            List::Node {
+                data: data,
+                next: Box::new(self),
+            }
+        }
+
+        // 不変イテレーターを返す
+        fn iter<'a>(&'a self) -> ListIter<'a, T> {
+            ListIter { elm: self }
+        }
+    }
+
+    // 不変イテレーターの型
+    struct ListIter<'a, T> {
+        elm: &'a List<T>,
+    }
+    impl<'a, T> Iterator for ListIter<'a, T> {
+        type Item = &'a T;
+
+        // 次の要素を返す
+        fn next(&mut self) -> Option<Self::Item> {
+            match self.elm {
+                List::Node { data, next } => {
+                    self.elm = next;
+                    Some(data)
+                }
+                List::Nil => None,
+            }
+        }
+    }
 }
