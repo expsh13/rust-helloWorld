78b35e2546bbe0593cbc4fdadb908f2588c2e702
Satoki Hata <satokihata@gmail.com>
2024-12-29 10:17:17 +0900

    トレイト

--- textbook/src/main.rs
+++ textbook/src/main.rs
@@ -1,10 +1,38 @@
-use std::ops::Mul;
-
 fn main() {
-    fn square<T>(x: T) -> T
-    where
-        T: Mul<Output = T> + Copy,
-    {
-        x * x
+    trait Location {
+        fn address(&self) -> &str;
     }
+
+    trait Person {
+        fn name(&self) -> &str;
+    }
+
+    trait House: Location + Person {}
+
+    fn print_house_info(house: &dyn House) {
+        println!("This house is located at {}", house.address());
+        println!("The owner of this house is {}", house.name());
+    }
+
+    struct MyHouse {
+        address: String,
+        owner: String,
+    }
+    impl Location for MyHouse {
+        fn address(&self) -> &str {
+            &self.address
+        }
+    }
+    impl Person for MyHouse {
+        fn name(&self) -> &str {
+            &self.owner
+        }
+    }
+    impl House for MyHouse {}
+
+    let my_house = MyHouse {
+        address: "123 Main St".to_string(),
+        owner: "John Doe".to_string(),
+    };
+    print_house_info(&my_house);
 }
