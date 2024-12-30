4ec0f48cf9226f6a684e1aa646523aa550a9fc0c
Satoki Hata <satokihata@gmail.com>
2024-12-30 10:35:20 +0900

    アプデ

--- thumbnail-tool/src/main.rs
+++ thumbnail-tool/src/main.rs
@@ -1,3 +1,38 @@
+use clap::Parser;
+use std::{
+    fs::{create_dir_all, read_dir},
+    path::PathBuf,
+};
+
+#[derive(Parser)]
+struct Args {
+    input: PathBuf,
+    output: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    // 出力先フォルダ作成
+    create_dir_all(&args.output).unwrap();
+
+    let mut processed_count = 0;
+    for item in read_dir(&args.input).unwrap() {
+        let item = item.unwrap();
+        let input_path = item.path();
+        if input_path.is_dir() {
+            //    フォルダは処理しない
+            continue;
+        }
+
+        // 画像ファイルの読み込み
+        let img = image::open(&input_path);
+        if let Ok(img) = img {
+            let thumbnail = img.thumbnail(64, 64);
+            let output_path = args.output.join(input_path.file_name().unwrap());
+            thumbnail.save(output_path).unwrap();
+            processed_count += 1;
+        }
+    }
+
+    println!("Processed {} images", processed_count);
 }
